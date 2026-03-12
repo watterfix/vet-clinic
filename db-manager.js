44321a214debd7dcfd6bfde3998655bed485794b
watterfix <artemkingfox@gmail.com>
2026-03-13 02:09:30 +0300

    Update db-manager.js

--- db-manager.js
+++ db-manager.js
@@ -4,9 +4,8 @@
 
 // Убираем let, используем const
 const DB_MANAGER = {
-    // ВАШИ ПРАВИЛЬНЫЕ ДАННЫЕ
-    BIN_ID: '69b32b80b7ec241ddc6348dd',  // Исправлено!
-    API_KEY: '$2a$10$tBa4S2lF6C0Qua271by8BuxrbzFEdlyz0lYj/oK52ynRQPDFexbC2',
+    BIN_ID: '69b34770c3097a1dd51e35f3',  
+    API_KEY: '$2a$10$nh6Q.DQPxUy3JGi6.fEL3e6DOeE02iVHQ1FBZ7N5FeF8sW0atefpK',
     BASE_URL: 'https://api.jsonbin.io/v3',
     
     currentData: {
